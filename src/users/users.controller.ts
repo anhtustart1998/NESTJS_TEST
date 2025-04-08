1440741dd4716f06b03115cc8632990f3a0ced5d
Tula <tutrang1998@gmail.com>
2025-04-08 12:45:38 +0700

    Add lession 3

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,31 +1,57 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
 @Controller('users') // /users
 // This controller handles requests to the /users endpoint
 export class UsersController {
-    @Get() // GET /users or /users?role=value
-    // The @Get() decorator is used to define a route for the GET HTTP method.
-    findAll(@Query('role') role?: 'INTERN' | 'ADMIN') {
-        return []
-    }
+  // The UsersController class is decorated with @Controller('users'), which means it will handle requests to the /users endpoint.
+  constructor(private readonly usersService: UsersService) {} // Injects the UsersService into the controller
+  @Get() // GET /users or /users?role=value
+  // The @Get() decorator is used to define a route for the GET HTTP method.
+  findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
+    return this.usersService.findAll(role); // Calls the findAll method of the UsersService to get all users or filter by role
+  }
 
-    @Get(':id') // GET /users/:id
-    findOne( @Param('id') id: string) {
-        return { id }
-    }
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(+id); // Calls the findOne method of the UsersService to get a user by ID
+  }
 
-    @Post() // POST /users
-    create(@Body() users: {}) {
-        return users
-    }
+  @Post() // POST /users
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user); // Calls the create method of the UsersService to create a new user
+  }
 
-    @Patch(':id') //PATCH /users/:id
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-        return {id, ...userUpdate}
-    }
+  @Patch(':id') //PATCH /users/:id
+  update(
+    @Param('id') id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.update(+id, userUpdate); // Calls the update method of the UsersService to update a user by ID
+  }
 
-    @Delete(':id') // DELETE /users/:id
-    delete(@Param('id') id: string) {
-        return { id }
-    }
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return this.usersService.delete(+id); // Calls the delete method of the UsersService to delete a user by ID
+  }
 }
